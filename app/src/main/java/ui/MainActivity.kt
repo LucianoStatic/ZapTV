8b3139dd2f4f2977bd3047e85ac22a689cd4abce
Luciano Drop <lucianojavax@gmail.com>
2022-04-19 02:28:11 -0300

    Iniciando o desenvolvimento da tela home

--- app/src/main/java/ui/MainActivity.kt
+++ app/src/main/java/ui/MainActivity.kt
@@ -1,12 +1,41 @@
 package ui
 
+import AdapterViewPager.ViewPagerAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.myapp.zaptv.R
+import com.myapp.zaptv.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        configTabLayout()
+    }
+
+
+    private fun configTabLayout() {
+
+        val adapter = ViewPagerAdapter(this)
+        binding.viewPager.adapter = adapter
+        adapter.addFragment(MovieFragment(), "Filmes")
+        adapter.addFragment(SerieFragment(), "Séries")
+        adapter.addFragment(FavoriteFragment(), "Favoritos")
+
+        binding.viewPager.offscreenPageLimit = adapter.itemCount
+        val mediator = TabLayoutMediator(
+            binding.tabs,
+            binding.viewPager
+        ) { tab: TabLayout.Tab, position: Int ->
+            tab.text = adapter.getTitle(position)
+        }
+        mediator.attach()
     }
 }
