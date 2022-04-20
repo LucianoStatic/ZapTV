99372a78135790602253e2b4eb3df4ce716b59eb
Luciano Drop <lucianojavax@gmail.com>
2022-04-19 23:46:05 -0300

    adicionando métodos do AnimatorListener

--- app/src/main/java/ui/SplashScreen.kt
+++ app/src/main/java/ui/SplashScreen.kt
@@ -1,12 +1,21 @@
 package ui
 
+import android.animation.Animator
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieAnimationView
 import com.myapp.zaptv.R
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
-class SplashScreen : AppCompatActivity() {
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity(){
+
+    private lateinit var lottieView: LottieAnimationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +26,26 @@ class SplashScreen : AppCompatActivity() {
             finish()
         }, 3000)
 
+        lottieView.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {
+
+                Log.d("AA", "animation start")
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+                Log.d("AA", "end of animation")
+            }
+
+            override fun onAnimationCancel(animation: Animator) {
+                Log.d("AA", "animation cancel")
+            }
+
+            override fun onAnimationRepeat(animation: Animator) {
+
+                Log.d("AA", "animation executes attribute animation every time it is called")
+            }
+        })
     }
+
 
 }
