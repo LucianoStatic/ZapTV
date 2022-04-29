fbdf094500b381713e43e70c8d8baac5aa76c92f
Luciano Drop <lucianojavax@gmail.com>
2022-04-29 15:17:25 -0300

    correção do itemMovie.xml e implementação dos componentes na fragment de detalhes dos filmes

--- app/src/main/java/util/ConvertData.kt
+++ app/src/main/java/util/ConvertData.kt
@@ -4,7 +4,7 @@ class ConvertData {
     companion object{
         fun toDate(date: String?): String {
             val month =
-                "Januari|Februari|Maret|April|Mei|Juni|Juli|Agustus|September|Oktober|November|Desember"
+                "Janeiro|Fevereiro|Março|Abril|Maio|Junho|Julho|Agosto|Setembro|Outubro|Novembro|Dezembro"
             val arrayMonth = month.split('|')
             val arrDate = date!!.split('-')
             return arrDate[2] + " " + arrayMonth[arrDate[1].toInt() - 1] + " " + arrDate[0]
